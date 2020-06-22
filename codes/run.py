964240b653632ff29503b39cac1148560deb2c12
Vaibhav <vaibhavadlakha95@gmail.com>
2020-06-22 13:33:04 +0530

    MINOR

--- codes/run.py
+++ codes/run.py
@@ -42,23 +42,23 @@ def parse_args(args=None):
     parser.add_argument('--regions', type=int, nargs='+', default=None,
                         help='Region Id for Countries S1/S2/S3 datasets, DO NOT MANUALLY SET')
 
-    parser.add_argument('--data_path', type=str, default=None)
-    parser.add_argument('--model', default='TransE', type=str)
+    parser.add_argument('--data_path', type=str, default='data/FB15k')
+    parser.add_argument('--model', default='RotatE', type=str)
     parser.add_argument('-de', '--double_entity_embedding',
                         action='store_true')
     parser.add_argument(
         '-dr', '--double_relation_embedding', action='store_true')
 
-    parser.add_argument('-n', '--negative_sample_size', default=128, type=int)
+    parser.add_argument('-n', '--negative_sample_size', default=256, type=int)
     parser.add_argument('-d', '--hidden_dim', default=500, type=int)
-    parser.add_argument('-g', '--gamma', default=12.0, type=float)
+    parser.add_argument('-g', '--gamma', default=24.0, type=float)
     parser.add_argument(
         '-adv', '--negative_adversarial_sampling', action='store_true')
     parser.add_argument('-a', '--adversarial_temperature',
                         default=1.0, type=float)
     parser.add_argument('-b', '--batch_size', default=1024, type=int)
     parser.add_argument('-r', '--regularization', default=0.0, type=float)
-    parser.add_argument('--test_batch_size', default=4,
+    parser.add_argument('--test_batch_size', default=10,
                         type=int, help='valid/test batch size')
     parser.add_argument('--uni_weight', action='store_true',
                         help='Otherwise use subsampling weighting like in word2vec')
@@ -69,7 +69,7 @@ def parse_args(args=None):
     parser.add_argument('-cpu', '--cpu_num', default=10, type=int)
     parser.add_argument('-init', '--init_checkpoint', default=None, type=str)
     parser.add_argument('-save', '--save_path', default='models', type=str)
-    parser.add_argument('--max_steps', default=100000, type=int)
+    parser.add_argument('--max_steps', default=150000, type=int)
     parser.add_argument('--warm_up_steps', default=None, type=int)
 
     parser.add_argument('--save_checkpoint_steps', default=10000, type=int)
